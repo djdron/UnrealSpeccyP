c2ad8bfee9f2acb76e5a730a74c07eebd1ca4434
Scorpion <scorpizx@gmail.com>
2010-01-22 21:12:55 +0000

    + snapshot 128k + 128k memory bugfix

--- snapshot.cpp
+++ snapshot.cpp
@@ -6,7 +6,7 @@
 #include "speccy.h"
 
 #pragma pack(push, 1)
-static struct eSnapshot48
+static struct eSnapshot
 {
 	byte i;
 	word alt_hl, alt_de, alt_bc, alt_af;
@@ -17,13 +17,18 @@ static struct eSnapshot48
 	byte im, pFE;
 	byte page5[eMemory::PAGE_SIZE];	// 4000-7FFF
 	byte page2[eMemory::PAGE_SIZE];	// 8000-BFFF
-	byte page0[eMemory::PAGE_SIZE];	// C000-FFFF
-} snapshot48;
+	byte page[eMemory::PAGE_SIZE];	// C000-FFFF
+	// 128k extension
+	word pc;
+	byte p7FFD;
+	byte trdos;
+	byte pages[5 * eMemory::PAGE_SIZE]; // all other pages
+} snapshot;
 #pragma pack(pop)
 
 struct eZ80Snap : public xZ80::eZ80
 {
-	void SetState(eSnapshot48* s)
+	void SetState(const eSnapshot* s, bool sna48)
 	{
 		alt.af = s->alt_af;
 		alt.bc = s->alt_bc;
@@ -36,18 +41,37 @@ struct eZ80Snap : public xZ80::eZ80
 		ix = s->ix;
 		iy = s->iy;
 		sp = s->sp;
+		pc = s->pc;
 		i = s->i;
 		r_low = s->r;
 		r_hi = s->r & 0x80;
 		im = s->im;
 		iff1 = s->iff1 ? 1 : 0;
-		pc = memory->Read(sp) + 0x100 * memory->Read(sp+1);
-		sp += 2;
 
 		devices->IoWrite(0xfe, s->pFE);
-		memcpy(memory->Get(eMemory::P_RAM5 * eMemory::PAGE_SIZE), s->page5, eMemory::PAGE_SIZE);
-		memcpy(memory->Get(eMemory::P_RAM2 * eMemory::PAGE_SIZE), s->page2, eMemory::PAGE_SIZE);
-		memcpy(memory->Get(eMemory::P_RAM0 * eMemory::PAGE_SIZE), s->page0, eMemory::PAGE_SIZE);
+		int p_size = eMemory::PAGE_SIZE;
+		memcpy(memory->Get(eMemory::P_RAM5 * p_size), s->page5, p_size);
+		memcpy(memory->Get(eMemory::P_RAM2 * p_size), s->page2, p_size);
+		int p = !sna48 ? (s->p7FFD & 7) : 0;
+		memcpy(memory->Get((eMemory::P_RAM0 + p) * p_size), s->page, p_size);
+
+		if(sna48)
+		{
+			pc = memory->Read(sp) + 0x100 * memory->Read(sp+1);
+			sp += 2;
+			return;
+		}
+		devices->IoWrite(0x7ffd, s->p7FFD);
+		const byte* page = s->pages;
+		byte mapped = 0x24 | (1 << (s->p7FFD & 7));
+		for(int i = 0; i < 8; ++i)
+		{
+			if(!(mapped & (1 << i)))
+			{
+				memcpy(memory->Get((eMemory::P_RAM0 + i) * p_size), page, p_size);
+				page += p_size;
+			}
+		}
 	}
 };
 
@@ -58,8 +82,9 @@ void xSnapshot::Load(eSpeccy* speccy, const char* path)
 {
 	FILE* f = fopen(path, "rb");
 	assert(f);
-	size_t r = fread(&snapshot48, 1, sizeof(snapshot48), f);
-	assert(r == sizeof(snapshot48));
+	size_t r = fread(&snapshot, 1, sizeof(snapshot), f);
 	fclose(f);
-	((eZ80Snap*)speccy->CPU())->SetState(&snapshot48);
+	bool sna48 = r == 49179;
+	assert(sna48 || r == sizeof(snapshot));
+	((eZ80Snap*)speccy->CPU())->SetState(&snapshot, sna48);
 }
