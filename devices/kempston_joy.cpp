72b1cec7a36120776dd62a9d35a15a1531848774
Andrey Dj <djdron@gmail.com>
2010-02-07 22:53:24 +0000

    kempston joystick support by cursor keys

---
+++ devices/kempston_joy.cpp
@@ -0,0 +1,46 @@
+#include "../std.h"
+#include "kempston_joy.h"
+
+void eKempstonJoy::Init() { Reset(); }
+void eKempstonJoy::Reset() { state = 0; }
+
+void eKempstonJoy::IoRead(word port, byte* v, int tact)
+{
+	if(port & 0x20)
+		return;
+	*v = state;
+}
+
+struct eButton
+{
+	char key;
+	byte bit;
+};
+enum { BUTTONS_COUNT = 5 };
+static const eButton buttons[BUTTONS_COUNT] =
+{
+	{'r',	0x01 },
+	{'l',	0x02 },
+	{'d',	0x04 },
+	{'u',   0x08 },
+	{'f',	0x10 },
+};
+void eKempstonJoy::OnKey(char _key, bool _down, bool _shift, bool _ctrl, bool _alt)
+{
+	KeyState(_key, _down);
+}
+void eKempstonJoy::KeyState(char _key, bool _down)
+{
+	for(int i = 0; i < BUTTONS_COUNT; ++i)
+	{
+		const eButton& b = buttons[i];
+		if(_key == b.key)
+		{
+			if(_down)
+				state |= b.bit;
+			else
+				state &= ~b.bit;
+			break;
+		}
+	}
+}
---
+++ devices/kempston_joy.h
@@ -0,0 +1,22 @@
+#ifndef __KEMPSTON_JOY_H__
+#define __KEMPSTON_JOY_H__
+
+#include "device.h"
+
+#pragma once
+
+class eKempstonJoy : public eDevice
+{
+public:
+	virtual void Init();
+	virtual void Reset();
+	virtual void IoRead(word port, byte* v, int tact);
+	void OnKey(char key, bool down, bool shift, bool ctrl, bool alt);
+
+	static eDeviceId Id() { return D_KEMPSTON_JOY; }
+protected:
+	void KeyState(char key, bool down);
+	byte state;
+};
+
+#endif//__KEMPSTON_JOY_H__
