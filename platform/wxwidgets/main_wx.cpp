eb9c67565970a32e77ae9653855c97a16a035b61
Andrey Dj <djdron@gmail.com>
2010-05-29 08:02:41 +0000

    + about box info added

--- platform/wxwidgets/main_wx.cpp
+++ platform/wxwidgets/main_wx.cpp
@@ -354,6 +354,21 @@ public:
 		info.SetVersion(_("0.0.9"));
 		info.SetDescription(_("Portable ZX-Spectrum emulator."));
 		info.SetCopyright(_("Copyright (C) 2001-2010 SMT, Dexus, Alone Coder, deathsoft, djdron, scor."));
+		info.SetWebSite(_("http://code.google.com/p/unrealspeccyp/"));
+		info.SetLicense(_(
+"This program is free software: you can redistribute it and/or modify\n\
+it under the terms of the GNU General Public License as published by\n\
+the Free Software Foundation, either version 3 of the License, or\n\
+(at your option) any later version.\n\
+\n\
+This program is distributed in the hope that it will be useful,\n\
+but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
+GNU General Public License for more details.\n\
+\n\
+You should have received a copy of the GNU General Public License\n\
+along with this program.  If not, see <http://www.gnu.org/licenses/>.\n"
+				));
 		wxAboutBox(info);
 	}
 #endif//_MAC
