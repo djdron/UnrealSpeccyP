4b5732f6ede9415a108b332f7867c0a9acc0c16f
Andrey Dj <djdron@gmail.com>
2013-02-27 23:14:11 +0000

    + zxaaa demos started from digits added + bugfix with 'Z' letter

--- build/android/src/app/usp/fs/FileSelectorZXAAA.java
+++ build/android/src/app/usp/fs/FileSelectorZXAAA.java
@@ -139,14 +139,14 @@ public class FileSelectorZXAAA extends FileSelector
 			return GetItemsResult.OK;
 		}
 		private final String[] ITEMS2 = new String[]
-			{	"/A", "/B", "/C", "/D", "/E", "/F", "/G", "/H",
+			{	"/123", "/A", "/B", "/C", "/D", "/E", "/F", "/G", "/H",
 				"/I", "/J", "/K", "/L", "/M", "/N", "/O", "/P", "/Q",
 				"/R", "/S", "/T", "/U", "/V", "/W", "/X", "/Y", "/Z"
 			};
 		private final String[] ITEMS2URLS = new String[]
-		    {	"a", "b", "c", "d", "e", "f", "g", "h",
+		    {	"@", "a", "b", "c", "d", "e", "f", "g", "h",
 				"i", "j", "k", "l", "m", "n", "o", "p", "q",
-				"r", "s", "t", "u", "v", "w", "x", "y", "/z"
+				"r", "s", "t", "u", "v", "w", "x", "y", "z"
 		    };
 	}
 }
