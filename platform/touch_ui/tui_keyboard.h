88f8abdaf2b9ed91c8d4a418d99b752e39030970
djdron <djdron@gmail.com>
2020-06-29 12:46:56 +0300

    + android build native code with CMake

--- platform/touch_ui/tui_keyboard.h
+++ platform/touch_ui/tui_keyboard.h
@@ -21,10 +21,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#ifdef _ANDROID
-#define USE_TUI
-#endif//_ANDROID
-
 #ifdef USE_TUI
 
 namespace xPlatform
