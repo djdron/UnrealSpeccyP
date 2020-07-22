219337c680484be0e69bf48347c1cecb9bc44ea5
djdron <djdron@gmail.com>
2020-07-23 01:24:38 +0300

    + wxWidgets OpenAL Soft build artifact added

--- build/appveyor/build.cmd
+++ build/appveyor/build.cmd
@@ -30,25 +30,31 @@ echo Found 7-Zip at !7ZIP!
 
 cd ../cmake
 
-rem goto :build_sdl2
+for %%I in (.) do set CUR_DIR=%%~dpnI
 
-rem build library
+rem build library---------------------------------------------------------------
+
 set BUILD_DIR=build_win32_library
 set BUILD_ARGS=-DUSE_SDL=0 -DUSE_LIBRARY=1
 call :build
 
-rem build benchmark
+
+rem build benchmark-------------------------------------------------------------
+
 set BUILD_DIR=build_win32_benchmark
 set BUILD_ARGS=-DUSE_SDL=0 -DUSE_BENCHMARK=1
 call :build
 
-:build_sdl2
-rem build sdl2
+
+rem build sdl2 gles2 angle curl-------------------------------------------------
+
 set SDL2LIB=SDL2-devel-2.0.12-VC.zip
 set URL=https://www.libsdl.org/release/%SDL2LIB%
 set DEST=%SDL2LIB%
 call :download
 if errorlevel 1 exit /b 1
+set SDL2LIB_DIR=%CUR_DIR%\SDL2-2.0.12
+del %SDL2LIB_DIR% /S /Q /F>nul
 !7ZIP! x %SDL2LIB%
 
 set ANGLELIB=angle-chromium84.7z
@@ -56,6 +62,8 @@ set URL=https://github.com/djdron/UnrealSpeccyP/releases/download/angle-chromium
 set DEST=%ANGLELIB%
 call :download
 if errorlevel 1 exit /b 1
+set ANGLELIB_DIR=%CUR_DIR%\angle-chromium84
+del %ANGLELIB_DIR% /S /Q /F>nul
 !7ZIP! x %ANGLELIB%
 
 set CURLLIB=curl-7.61.1.7z
@@ -63,23 +71,41 @@ set URL=https://github.com/djdron/UnrealSpeccyP/releases/download/angle-chromium
 set DEST=%CURLLIB%
 call :download
 if errorlevel 1 exit /b 1
+set CURLLIB_DIR=%CUR_DIR%\curl-7.61.1
+del %CURLLIB_DIR% /S /Q /F>nul
 !7ZIP! x %CURLLIB%
 
-for %%I in (.) do set CUR_DIR=%%~dpnI
-set SDL2LIB_DIR=%CUR_DIR%/SDL2-2.0.12
-set ANGLELIB_DIR=%CUR_DIR%/angle-chromium84
-set CURLLIB_DIR=%CUR_DIR%/curl-7.61.1
 set BUILD_DIR=build_win32_sdl2
 set ANGLE_ARGS=-DANGLE_INCLUDE_DIR=%ANGLELIB_DIR%/include -DANGLE_LIBRARY=%ANGLELIB_DIR%/lib/x64/libGLESv2.lib;%ANGLELIB_DIR%/lib/x64/libEGL.lib
 set SDL2_ARGS=-DSDL2_INCLUDE_DIRS=%SDL2LIB_DIR%/include -DSDL2_LIBRARIES=%SDL2LIB_DIR%/lib/x64/SDL2main.lib;%SDL2LIB_DIR%/lib/x64/SDL2.lib
 set CURL_ARGS=-DCURL_INCLUDE_DIR=%CURLLIB_DIR%/include -DCURL_LIBRARY=%CURLLIB_DIR%/lib/x64/libcurl.lib
 set BUILD_ARGS=-DUSE_SDL=0 -DUSE_SDL2=1 %ANGLE_ARGS% %SDL2_ARGS% %CURL_ARGS%
-echo %BUILD_ARGS%
+call :build
+
+
+rem build wxWidgets OpenAL Soft-------------------------------------------------
+
+set OPENALLIB=openal-soft-1.20.1-bin.zip
+set URL=https://openal-soft.org/openal-binaries/%OPENALLIB%
+set DEST=%OPENALLIB%
+call :download
+if errorlevel 1 exit /b 1
+set OPENALLIB_DIR=%CUR_DIR%\openal-soft-1.20.1-bin
+del %OPENALLIB_DIR% /S /Q /F>nul
+!7ZIP! x %OPENALLIB%
+
+set OPENALDIR=%OPENALLIB_DIR%
+set BUILD_DIR=build_win32_wxwidgets
+set BUILD_ARGS=-DUSE_SDL=0 -DUSE_WX_WIDGETS=1
 call :build
 
 exit /b
 
+
 :build
+
+rem CMake build
+
 del %BUILD_DIR% /S /Q /F>nul
 mkdir %BUILD_DIR%
 pushd %BUILD_DIR%
@@ -88,10 +114,13 @@ cmake --build . --config Release
 popd
 exit /b
 
+
 :download
 
 rem Download !URL! to !DEST!.
 rem Note: Positional parameters don't work here, as the percent signs in URLs are eagerly interpreted despite quoting.
+
+echo Downloading !URL!
 
 for %%a in (powershell.exe) do if not [%%~$PATH:a] == [] powershell -Command "(New-Object Net.WebClient).DownloadFile('!URL!', '!DEST!')" & goto :eof
 for %%a in (curl.exe) do if not [%%~$PATH:a] == [] curl "!URL!" -O "!DEST!" --location & goto :eof
