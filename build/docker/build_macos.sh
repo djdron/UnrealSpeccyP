4b649c62c22a46203a85b5da0b01841ea3a1969a
djdron <djdron@gmail.com>
2021-04-04 01:02:50 +0300

    + appveyor docker build fix

--- build/docker/build_macos.sh
+++ build/docker/build_macos.sh
@@ -2,5 +2,5 @@
 
 cd $(dirname "$0")
 docker build -t build-macos ./macOS
-docker run --name build-macos -it build-macos
+docker run --name build-macos build-macos
 docker cp build-macos:/build/UnrealSpeccyPortable.dmg ./unreal-speccy-portable_0.0.90_x86_64_sdl2_gles2_angle_curl.dmg
