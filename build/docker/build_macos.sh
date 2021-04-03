db41e73f425d8b52dedd4b85b08099537c4b540c
djdron <djdron@gmail.com>
2021-04-04 00:53:54 +0300

    + appveyor docker build experiment

--- build/docker/build_macos.sh
+++ build/docker/build_macos.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd $(dirname "$0")
 docker build -t build-macos ./macOS
 docker run --name build-macos -it build-macos
 docker cp build-macos:/build/UnrealSpeccyPortable.dmg ./unreal-speccy-portable_0.0.90_x86_64_sdl2_gles2_angle_curl.dmg
