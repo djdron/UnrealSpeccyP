faab1b7907a9b9d7fb610cc9b785c4e869e8a4e0
djdron <djdron@gmail.com>
2022-12-09 01:27:22 +0200

    + macOS universal (arm64-x86_64) build

--- build/docker/macOS/build.sh
+++ build/docker/macOS/build.sh
@@ -1,25 +1,36 @@
 #!/bin/bash
 
-tar -xJf ./angle-chromium89.tar.xz
+tar -xJf ./angle-chromium108.tar.xz
 tar -xJf ./dmg-template.tar.xz
 
-git clone https://bitbucket.org/djdron/unrealspeccyp.git usp
+#workaround ripemd160 in openssl
+sed -i 's/\bopenssl rmd160\b/openssl rmd160 -provider legacy/g' $(command -v osxcross-macports)
+sed -i 's/\bopenssl dgst -ripemd160 -verify\b/openssl dgst -provider default -provider legacy -ripemd160 -verify/g' $(command -v osxcross-macports)
+
+#workaround universal binaries with arm64, not i386
+sed -i 's/ARCH="i386-x86_64"/ARCH="arm64-x86_64"/g' $(command -v osxcross-macports)
+
+export MACOSX_DEPLOYMENT_TARGET=11.0
+
+osxcross-macports install libsdl2 -universal
+
+git clone https://github.com/djdron/UnrealSpeccyP.git usp
 cd usp/build/cmake
 mkdir build_macos
 cd build_macos
-export OSXCROSS_HOST=x86_64-apple-darwin19
-export GLES2_PATH=/build/angle-chromium89
+export OSXCROSS_HOST=x86_64-apple-darwin21.4
+export GLES2_PATH=/build/angle-chromium108
 export GLES2_INCLUDE_PATH=$GLES2_PATH/include
-export GLES2_LIB_PATH=$GLES2_PATH/lib/macOS/x64
-osxcross-conf
-cmake .. -G Ninja -DUSE_SDL2=1 -DUSE_SDL=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/osxcross/target/toolchain.cmake -DOPENGLES2_INCLUDE_DIR=$GLES2_INCLUDE_PATH -DOPENGLES2_gl_LIBRARY=$GLES2_LIB_PATH/libGLESv2.dylib
+export GLES2_LIB_PATH=$GLES2_PATH/lib/macOS
+eval `osxcross-conf`
+cmake .. -G Ninja -DUSE_SDL2=1 -DUSE_SDL=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/osxcross/toolchain.cmake -DOPENGLES2_INCLUDE_DIR=$GLES2_INCLUDE_PATH -DOPENGLES2_gl_LIBRARY=$GLES2_LIB_PATH/libGLESv2.dylib
 cmake --build . -- -j 4
 
 cd UnrealSpeccyPortable.app/Contents
 mkdir lib
 cd lib
 cp $GLES2_LIB_PATH/* .
-cp /osxcross/target/macports/pkgs/opt/local/lib/libSDL2-2.0.0.dylib .
+cp /osxcross/macports/pkgs/opt/local/lib/libSDL2-2.0.0.dylib .
 export OTOOL=$OSXCROSS_HOST-otool
 export INTOOL=$OSXCROSS_HOST-install_name_tool
 $INTOOL -id @rpath/libEGL.dylib ./libEGL.dylib
