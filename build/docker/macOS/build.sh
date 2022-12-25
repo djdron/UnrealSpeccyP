21f28ee7525cc2335092859ae30af7abe95ba10d
djdron <djdron@gmail.com>
2022-12-25 23:26:57 +0200

    + build for x86_64 because of signing/notarizing and other shit for arm64

--- build/docker/macOS/build.sh
+++ build/docker/macOS/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-tar -xJf ./angle-chromium108.tar.xz
+tar -xJf ./angle-chromium89.tar.xz
+#tar -xJf ./angle-chromium108.tar.xz
 tar -xJf ./dmg-template.tar.xz
 
 #workaround ripemd160 in openssl
@@ -10,20 +11,25 @@ sed -i 's/\bopenssl dgst -ripemd160 -verify\b/openssl dgst -provider default -pr
 #workaround universal binaries with arm64, not i386
 sed -i 's/ARCH="i386-x86_64"/ARCH="arm64-x86_64"/g' $(command -v osxcross-macports)
 
-export MACOSX_DEPLOYMENT_TARGET=12.0
+export MACOSX_DEPLOYMENT_TARGET=10.9
+#export MACOSX_DEPLOYMENT_TARGET=12.0
 
-osxcross-macports install libsdl2 -universal
+osxcross-macports install libsdl2
+#osxcross-macports install libsdl2 -universal
 
 git clone https://github.com/djdron/UnrealSpeccyP.git usp
 cd usp/build/cmake
 mkdir build_macos
 cd build_macos
 export OSXCROSS_HOST=x86_64-apple-darwin21.4
-export GLES2_PATH=/build/angle-chromium108
+export GLES2_PATH=/build/angle-chromium89
+#export GLES2_PATH=/build/angle-chromium108
 export GLES2_INCLUDE_PATH=$GLES2_PATH/include
-export GLES2_LIB_PATH=$GLES2_PATH/lib/macOS
+export GLES2_LIB_PATH=$GLES2_PATH/lib/macOS/x64
+#export GLES2_LIB_PATH=$GLES2_PATH/lib/macOS
 eval `osxcross-conf`
-cmake .. -G Ninja -DUSE_SDL2=1 -DUSE_SDL=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/osxcross/toolchain.cmake -DOPENGLES2_INCLUDE_DIR=$GLES2_INCLUDE_PATH -DOPENGLES2_gl_LIBRARY=$GLES2_LIB_PATH/libGLESv2.dylib
+cmake .. -G Ninja -DUSE_SDL2=1 -DUSE_SDL=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/osxcross/toolchain.cmake -DCMAKE_OSX_ARCHITECTURES="x86_64" -DOPENGLES2_INCLUDE_DIR=$GLES2_INCLUDE_PATH -DOPENGLES2_gl_LIBRARY=$GLES2_LIB_PATH/libGLESv2.dylib
+#-DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
 cmake --build . -- -j 4
 
 cd UnrealSpeccyPortable.app/Contents
