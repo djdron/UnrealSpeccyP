02853d65f027dec2fe72749865e288c5197bd6c1
djdron <djdron@gmail.com>
2023-10-01 20:49:47 +0200

    + osxcross build fix with latest toolchain

--- build/docker/macOS/build.sh
+++ build/docker/macOS/build.sh
@@ -21,14 +21,14 @@ git clone https://github.com/djdron/UnrealSpeccyP.git usp
 cd usp/build/cmake
 mkdir build_macos
 cd build_macos
-export OSXCROSS_HOST=x86_64-apple-darwin21.4
+export OSXCROSS_HOST=x86_64-apple-darwin22.2
 export GLES2_PATH=/build/angle-chromium89
 #export GLES2_PATH=/build/angle-chromium108
 export GLES2_INCLUDE_PATH=$GLES2_PATH/include
 export GLES2_LIB_PATH=$GLES2_PATH/lib/macOS/x64
 #export GLES2_LIB_PATH=$GLES2_PATH/lib/macOS
-eval `osxcross-conf`
-cmake .. -G Ninja -DUSE_SDL2=1 -DUSE_SDL=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=/osxcross/toolchain.cmake -DCMAKE_OSX_ARCHITECTURES="x86_64" -DOPENGLES2_INCLUDE_DIR=$GLES2_INCLUDE_PATH -DOPENGLES2_gl_LIBRARY=$GLES2_LIB_PATH/libGLESv2.dylib
+
+$OSXCROSS_HOST-cmake .. -G Ninja -DUSE_SDL2=1 -DUSE_SDL=0 -DCMAKE_BUILD_TYPE=Release -DCMAKE_OSX_ARCHITECTURES="x86_64" -DOPENGLES2_INCLUDE_DIR=$GLES2_INCLUDE_PATH -DOPENGLES2_gl_LIBRARY=$GLES2_LIB_PATH/libGLESv2.dylib
 #-DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"
 cmake --build . -- -j 4
 
