10ba29ab858cf5a61f683f66121c6991bc8d2196
djdron <djdron@gmail.com>
2024-02-27 12:08:53 +0100

    + imgui inserted with demo window

--- platform/sdl2/sdl2.cpp
+++ platform/sdl2/sdl2.cpp
@@ -26,6 +26,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "../../tools/tick.h"
 #include "../io.h"
 
+#ifdef USE_IMGUI
+#include "../../3rdparty/imgui/backends/imgui_impl_sdl2.h"
+#include "../../3rdparty/imgui/backends/imgui_impl_opengl3.h"
+#endif//USE_IMGUI
+
 namespace xPlatform
 {
 
@@ -193,6 +198,7 @@ void Loop1()
 	SDL_Event e;
 	while(SDL_PollEvent(&e))
 	{
+		if(!ImGui_ImplSDL2_ProcessEvent(&e))
 		switch(e.type)
 		{
 		case SDL_QUIT:
