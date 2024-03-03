f7d6190083e93df94d4b032b6e8a04eaf9e6ab14
djdron <djdron@gmail.com>
2024-03-03 21:56:43 +0100

    + imgui replay info added

--- platform/sdl2/sdl2_imgui.cpp
+++ platform/sdl2/sdl2_imgui.cpp
@@ -54,6 +54,78 @@ void Done()
 	ImGui::DestroyContext();
 }
 
+static void ProcessReplayInfo()
+{
+	dword frame_current = 0, frames_total = 0, frames_cached = 0;
+	if(!xPlatform::Handler()->GetReplayProgress(&frame_current, &frames_total, &frames_cached) || !frames_total)
+		return;
+
+	char buf[16];
+	auto TimeToString = [&buf](int seconds)
+	{
+		int h = seconds / 3600;
+		int m = (seconds % 3600) / 60;
+		int s = seconds % 60;
+		if(h > 0)
+			snprintf(buf, sizeof buf, "%02d:%02d:%02d", h, m, s);
+		else
+			snprintf(buf, sizeof buf, "%02d:%02d", m, s);
+		return buf;
+	};
+
+	float alpha = 1.0f;
+
+	static ImVec2 smax = ImGui::CalcTextSize("88:88:88");
+	static ImVec2 smin = ImGui::CalcTextSize("88:88");
+	static float scenter = (smax.x - smin.x)/2.0f;
+
+	ImDrawList* draw_list = ImGui::GetBackgroundDrawList();
+	ImVec2 rect = draw_list->GetClipRectMax();
+
+	const char* time_current = TimeToString(frame_current/50);
+	float dx_current = (strlen(time_current) == 5) ? scenter : 0.0f;
+	draw_list->AddText(ImVec2(10 + dx_current, rect.y - smax.y*2), ImColor(1.0f, 1.0f, 1.0f, 1.0f*alpha), time_current);
+
+	const char* time_total = TimeToString(frames_total/50);
+	float dx_total = (strlen(time_total) == 5) ? scenter : 0.0f;
+	draw_list->AddText(ImVec2(rect.x - 10 - smax.x + dx_total, rect.y - smax.y*2), ImColor(1.0f, 1.0f, 1.0f, 1.0f*alpha), time_total);
+
+	float p = (float)frame_current/frames_total;
+	int width = rect.x - 40 - smax.x*2;
+	int px = 20 + smax.x;
+	int py = smax.y*3/2;
+	int psx = (int)(width*p);
+	draw_list->AddRectFilled(ImVec2(px, rect.y - py - 2), ImVec2(px + psx, rect.y - py + 3), ImColor(0.5f, 0.0f, 0.0f, 0.8f*alpha));
+	int psxc = psx;
+	if(frame_current + frames_cached < frames_total)
+	{
+		float pc = (float)(frame_current + frames_cached)/frames_total;
+		psxc = (int)(width*pc);
+		draw_list->AddRectFilled(ImVec2(px + psx, rect.y - py - 2), ImVec2(px + psxc, rect.y - py + 3), ImColor(0.4f, 0.4f, 0.4f, 0.8f*alpha));
+	}
+	draw_list->AddRectFilled(ImVec2(px + psxc, rect.y - py - 2), ImVec2(px + width, rect.y - py + 3), ImColor(0.2f, 0.2f, 0.2f, 0.8f*alpha));
+	draw_list->AddCircleFilled(ImVec2(px + psx, rect.y - py + 1), 5, ImColor(1.0f, 1.0f, 1.0f, 0.8f*alpha));
+}
+
+static void Process()
+{
+	static bool show_demo_window = true;
+	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+	if(show_demo_window)
+		ImGui::ShowDemoWindow(&show_demo_window);
+
+	static bool show_test = true;
+	if(show_test)
+	{
+		if(ImGui::Begin("test", &show_test))
+		{
+		}
+		ImGui::End();
+	}
+
+	ProcessReplayInfo();
+}
+
 void Update()
 {
 	// Start the Dear ImGui frame
@@ -61,10 +133,7 @@ void Update()
 	ImGui_ImplSDL2_NewFrame();
 	ImGui::NewFrame();
 
-	static bool show_demo_window = true;
-	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-	if(show_demo_window)
-		ImGui::ShowDemoWindow(&show_demo_window);
+	Process();
 
 	// Rendering
 	ImGui::Render();
